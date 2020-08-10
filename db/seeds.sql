f3e32de308aac52570e0fa7f61a7c84bdce45ea0
Justin Ramirez <ramirezbjustin@gmail.com>
2020-08-10 17:40:04 -0400

    adding in new file structure for application

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (name, ingredients) VALUES ('Its Fun to Eat at the rYeMCA Burger','two beef patties, lettuce topped with cheddar cheese, chunky guacmole, rye bread');
+INSERT INTO burgers (name, ingredients) VALUES ('If Looks Could Kale', 'beef patty, complex gruyere cheese and placed atop a bed of leafy garlic roasted kale, toasted all wheat bun');
+INSERT INTO burgers (name, ingredients) VALUES ('Beets Me Burger', 'sirloin Burger, balsamic honey, ginger beets, soft cherve');
+INSERT INTO burgers (name, ingredients) VALUES ('A Leek of Their Own Burger', 'feta and garlic lamb burger, braised leeks, mustard greens, dollop of potato-leek mustard sauce' );
+INSERT INTO burgers (name, ingredients) VALUES ('I Know Why The Burger Sings', 'sausage and beef burger, shrimp gumbo and rice, collard greens');
+INSERT INTO burgers (name, ingredients) VALUES ('Eggers Cannot Be Cheesers Burger', 'american cheese, one fried egg, hot sauce, piece of lettuce on a plain bun');
+INSERT INTO burgers (name, ingredients) VALUES ('I Heartichoke You Burger', 'beef patty, garlic and topped with artichokes, stewed tomatoes and parmesan cheese, on a toasted onion bun');
+INSERT INTO burgers (name, ingredients) VALUES ('One Horse Open Slaw Burger', 'beef patty marinated in Worcesterchire sauce, topped with melted jalapeño Havarti cheese, sweet onion bun');
+
+
